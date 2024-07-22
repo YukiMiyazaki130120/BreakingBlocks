721f971a8949f8b23bbe16b82108d0e6171bd565
YukiMiyazaki13120 <hyuki13120@gmail.com>
2024-07-22 23:58:11 +0900

    変更しましたで

---
+++ Assets/Script/index.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class index : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //右矢印を押している間右に動かす
+
+    }
+}
